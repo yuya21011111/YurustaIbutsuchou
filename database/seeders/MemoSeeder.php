69ada9c1af1bcdaae25a6c7fc8c48efa65cd4a14
yuya21011111 <kaimomo12345@gmail.com>
2025-04-24 18:26:52 +0900

    更新履歴

--- database/seeders/MemoSeeder.php
+++ database/seeders/MemoSeeder.php
@@ -15,6 +15,14 @@ class MemoSeeder extends Seeder
     public function run(): void
     {
         DB::table('memos')->insert([
+
+            [
+                'version' => 'v1.2',
+                'date' => '2025-04-24',
+                'memo' => 'API使用不可時のセーブモードに対応（DBに値を保持）',
+                'created_at' => Carbon::create('2025', '04', '08', '12', '00', '00'),
+                'updated_at' => Carbon::create('2025', '04', '08', '12', '00', '00'),
+            ],
             [
                 'version' => 'v1.1',
                 'date' => '2025-04-20',
